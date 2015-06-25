f94901d02ea9ee543b643b2033044febd2a867d9
Marc Debnicki <you@example.com>
2015-06-25 17:17:16 +0200

    Playing with checkout still

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
   def buy
     @product = Shoppe::Product.find_by_permalink!(params[:permalink])
     current_order.order_items.add_item(@product, 1)
-    redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
+    redirect_to product_path(@product.permalink), :notice => "Da meal is in your belly!"
   end
 
   def show
