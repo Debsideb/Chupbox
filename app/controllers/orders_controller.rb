ecba6cf85114adcdf10df7c7ba506ad3f0da0edb
Marc Debnicki <you@example.com>
2015-07-02 04:59:45 +0200

    Almost there man

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 
- def checkout
-  @order = Shoppe::Order.find(current_order.id)
+  def checkout
+    @order = Shoppe::Order.find(current_order.id)
     if request.patch?
       if @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
       redirect_to checkout_payment_path
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
 
   def payment
     if request.post?
-      redirect_to checkout_confirmation_path
+    redirect_to checkout_confirmation_path
     end
   end
 
@@ -19,7 +19,7 @@ class OrdersController < ApplicationController
     if request.post?
       current_order.confirm!
       session[:order_id] = nil
-      redirect_to root_path, :notice => "Nice, we received your order!"
+      redirect_to root_path, :notice => "Order has been placed successfully!"
     end
   end
 
